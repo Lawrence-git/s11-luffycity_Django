fab609fc0b9fe65ee850e07ecc5cce33584ab3b5
tielemao <tieleyumao@gmail.com>
2018-08-07 22:26:06 +0800

    路飞学城第二次提交

--- api/serializers/course.py
+++ api/serializers/course.py
@@ -5,7 +5,6 @@ class CourseSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField()
 
-
 class CourseModelSerializer(serializers.ModelSerializer):
     level_name = serializers.CharField(source='get_level_display')
     hours = serializers.CharField(source='coursedetail.hours')
@@ -21,3 +20,12 @@ class CourseModelSerializer(serializers.ModelSerializer):
     def get_recommend_courses(self,row):
         recommend_list = row.coursedetail.recommend_courses.all()
         return [ {'id':item.id,'name':item.name} for item in recommend_list]
+
+class DegreeCourseSerializer(serializers.ModelSerializer):
+
+    name = serializers.CharField(source='get_level_display')
+
+    class Meta:
+        model = models.DegreeCourse
+        fields = ['id','name']
+
--- api/views/course.py
+++ api/views/course.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.shortcuts import HttpResponse
 from django.http import JsonResponse
 
@@ -9,10 +9,10 @@ from rest_framework.response import Response
 from rest_framework.versioning import URLPathVersioning
 from rest_framework.pagination import PageNumberPagination
 
-from api.serializers.course import CourseSerializer,CourseModelSerializer
+from api.serializers.course import CourseSerializer, CourseModelSerializer, DegreeCourseSerializer
 from api import serializers as app01_serializers
 from api.utils.response import BaseResponse
-from api.models import models
+from api import models
 import json
 
 
@@ -23,19 +23,19 @@ import json
 
 class CoursesView(APIView):
 
-    def get(self,request,*args,**kwargs):
-        # response = {'code':1000,'data':None,'error':None}
+    def get(self, request, *args, **kwargs):
         ret = BaseResponse()
+
         try:
             # 从数据库获取数据
             queryset = models.Course.objects.all()
 
             # 分页
             page = PageNumberPagination()
-            course_list = page.paginate_queryset(queryset,request,self)
+            course_list = page.paginate_queryset(queryset, request, self)
 
             # 分页之后的结果执行序列化
-            ser = CourseModelSerializer(instance=course_list,many=True)
+            ser = CourseModelSerializer(instance=course_list, many=True)
 
             ret.data = ser.data
         except Exception as e:
@@ -57,3 +57,44 @@ class CourseDetailView(APIView):
             response['error'] = '获取数据失败'
         return Response(response)
 
+
+# a.查看所有学位课并打印学位课名称以及授课老师
+class DegreeCourseView(APIView):
+    """
+	查看所有学位课
+	"""
+
+    def get(self, request, *args, **kwargs):
+        ret = BaseResponse()
+
+    try:
+        # 从数据库获取数据
+        queryset = models.DegreeCourse.objects.all()
+
+        # 分页
+        page = PageNumberPagination()
+        degree_list = page.paginate_queryset(queryset, request, self)
+
+        # 分页之后的结果执行序列化
+        ser = DegreeCourseSerializer(instance=degree_list, many=True)
+        ret.data = ser.data
+
+    except Exception as e:
+        ret.code = 500
+        ret.error = '获取数据失败'
+
+    return Response(ret.dict)
+
+# b.查看所有学位课并打印学位课名称以及学位课的奖学金
+
+# c.展示所有的专题课
+
+# d. 查看id=1的学位课对应的所有模块名称
+
+# e.获取id = 1的专题课，并打印：课程名、级别(中文)、why_study、what_to_study_brief、所有recommend_courses
+
+# f.获取id = 1的专题课，并打印该课程相关的所有常见问题
+
+# g.获取id = 1的专题课，并打印该课程相关的课程大纲
+
+# h.获取id = 1的专题课，并打印该课程相关的所有章节
